a19c18c73f2754cb65080d80474ea13dc887aabb
Brayden <sv.brayden@outlook.com>
2020-08-31 14:55:41 -0400

    Change Avatar to use IonAvatar

--- src/components/dashboard/AvatarCard.tsx
+++ src/components/dashboard/AvatarCard.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { IonCard, IonItem, IonIcon, IonTitle } from '@ionic/react';
-import { personCircle } from 'ionicons/icons';
+import { IonCard, IonItem, IonTitle, IonAvatar } from '@ionic/react';
 
 const AvatarCard: React.FC = () => {
   return (
     <IonCard routerLink="/option">
         <IonItem>
-          <IonIcon icon={personCircle} />
+          <IonAvatar>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fiupac.org%2Fwp-content%2Fuploads%2F2018%2F05%2Fdefault-avatar.png" alt=""/>
+          </IonAvatar>
           <IonTitle>Logged In User</IonTitle>
         </IonItem>
     </IonCard>
