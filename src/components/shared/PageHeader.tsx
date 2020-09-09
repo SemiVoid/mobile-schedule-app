2f209cc0c47bb75018b584440d0f635b76181924
Brayden <sv.brayden@outlook.com>
2020-09-09 03:48:44 -0400

    Create Shared Page Header

---
+++ src/components/shared/PageHeader.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+
+interface PageHeaderProps {
+  title: string;
+  condense?: boolean;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({ title, condense}) => {
+  return (
+    <>
+      {!condense ? (
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>{title}</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+      ) : (
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">{title}</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+      )}
+    </>
+  );
+};
+
+export default PageHeader;
