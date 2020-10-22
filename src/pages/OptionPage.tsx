d3c339e34fecdb09bb1863f20c4a02c89f71c613
Brayden <sv.brayden@outlook.com>
2020-10-22 00:37:21 -0400

    Move Account Update Side Effect to Option Page

--- src/pages/OptionPage.tsx
+++ src/pages/OptionPage.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // Import Components
 import PageContainer from '../components/shared/Page/PageContainer';
 import AccountSection from '../components/pages/option/AccountSection';
 
+import { useDispatch } from 'react-redux';
+import { userAccount } from '../redux';
+import { auth } from '../config/firebase';
+
 const OptionPage: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const accountChange = () => {
+    const unsub = auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(userAccount({ account: user }));
+        console.log(`Account changed to ${user.email}`);
+      }
+    });
+
+    return unsub;
+  };
+
+  useEffect(accountChange, []);
+
   return (
     <PageContainer pageTitle="Settings" fullscreen>
       <AccountSection />
